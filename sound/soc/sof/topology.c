3c282db857998a21190f26caa7ba99a7c53bba26
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-01-09 13:33:39 -0800

    ASoC: SOF: add function name to the error messages
    There are 4 functions which could result in the same error
    message "failed to find dai". Add the function name
    in the error message so it is easy to tell which
    one failed.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/topology.c
+++ sound/soc/sof/topology.c
@@ -2100,7 +2100,8 @@ static int sof_link_hda_process(struct snd_sof_dev *sdev,
 	}
 
 	if (!found) {
-		dev_err(sdev->dev, "error: failed to find dai %s", link->name);
+		dev_err(sdev->dev, "error: failed to find dai %s in %s",
+			link->name, __func__);
 		return -EINVAL;
 	}
 
@@ -2150,8 +2151,8 @@ static int sof_link_hda_load(struct snd_soc_component *scomp, int index,
 	dai_component.dai_name = link->cpu_dai_name;
 	dai = snd_soc_find_dai(&dai_component);
 	if (!dai) {
-		dev_err(sdev->dev, "error: failed to find dai %s",
-			dai_component.dai_name);
+		dev_err(sdev->dev, "error: failed to find dai %s in %s",
+			dai_component.dai_name, __func__);
 		return -EINVAL;
 	}
 
@@ -2293,8 +2294,8 @@ static int sof_link_hda_unload(struct snd_sof_dev *sdev,
 	dai_component.dai_name = link->cpu_dai_name;
 	dai = snd_soc_find_dai(&dai_component);
 	if (!dai) {
-		dev_err(sdev->dev, "error: failed to find dai %s",
-			dai_component.dai_name);
+		dev_err(sdev->dev, "error: failed to find dai %s in %s",
+			dai_component.dai_name, __func__);
 		return -EINVAL;
 	}
 
@@ -2330,7 +2331,8 @@ static int sof_link_unload(struct snd_soc_component *scomp,
 			goto found;
 	}
 
-	dev_err(sdev->dev, "error: failed to find dai %s", link->name);
+	dev_err(sdev->dev, "error: failed to find dai %s in %s",
+		link->name, __func__);
 	return -EINVAL;
 found:
 
